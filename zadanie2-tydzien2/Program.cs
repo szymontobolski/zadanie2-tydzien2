97a26d1a5560c418969343b32061c4f168ec6e6e
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-18 16:30:57 +0200

    Add project files.

---
+++ zadanie2-tydzien2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadanie2_tydzien2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 2");
+            string zmienna1 = "A";
+            string zmienna2 = "B";
+            string zmienna3 = "C";
+
+            Console.WriteLine(zmienna3 + zmienna2 + zmienna1);
+
+        }
+    }
+}
